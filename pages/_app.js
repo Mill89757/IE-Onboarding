2793821afa15b8fb00888b3a11c47916c86986cd
Mill <yushuguo89757@gmail.com>
2023-07-29 17:24:30 +1000

    Feat: Init

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Monash IE Onboarding</h1>
+      <p>This is a new page in your Next.js app.</p>
+    </div>
+  );
+};
+
+export default Index;
