0da4c8fe479c1b822fc5c979c1a068b80c4c3670
Mill <yguo0035@student.monash.edu>
2023-08-06 17:19:01 +1000

    Update index page password setting
    Make sure the password is affected the whole page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import EntranceCard from "../src/components/EntranceCard";
 
 const Index = () => {
   const [showProtectedContent, setShowProtectedContent] = useState(false);
-  const [password, setPassword] = useState("panda"); 
+  const [password, setPassword] = useState("panda");
 
   const showContent = () => {
     const enteredPassword = prompt("Enter the password:");
@@ -18,10 +18,10 @@ const Index = () => {
 
   return (
     <>
-      <div>
-        <Header index={0} />
-        <div className="h-[calc(100vh_-_80px)]">
-          {showProtectedContent ? (
+      {showProtectedContent ? (
+        <div>
+          <Header index={0} />
+          <div className="h-[calc(100vh_-_80px)]">
             <div className="flex items-center justify-center gap-x-[86px] h-full">
               <EntranceCard
                 title="Sustainability "
@@ -34,13 +34,16 @@ const Index = () => {
                 link="/carboninfo"
               />
             </div>
-          ) : (
-            <div className="flex items-center justify-center h-full">
-              <button onClick={showContent}>Enter Password</button>
-            </div>
-          )}
+            )
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex h-screen w-screen bg-black justify-center items-center">
+          <div className="w-[200px] h-[44px] flex items-center justify-center bg-white rounded-md">
+            <button onClick={showContent}>Enter Password</button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
