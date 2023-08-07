c3e4ee20103eda65daf1d430d1957bfd44e33cf9
May Duan <may_duan@hotmail.com>
2023-08-07 20:10:17 +1000

    add formula

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,9 @@
 import CheckTool from "./CheckTool";
 import { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 export default function Header(props) {
   const index = props.index;
-  const [showProtectedContent, setShowProtectedContent] = useState(false);
-  const [password, setPassword] = useState("panda"); 
-  const router = useRouter();
-
-  const showContent = () => {
-    const enteredPassword = prompt("Enter the password:");
-    if (enteredPassword === password) {
-      setShowProtectedContent(true);
-      router.push(router.pathname); 
-    } else {
-      alert("Incorrect password. Access denied.");
-    }
-  };
-
-  const renderLink = (path, text) => {
-    if (showProtectedContent || index === path) {
-      return (
-        <Link href={path}>
-          <div
-            className={`flex w-[180px] items-center justify-center h-full cursor-pointer ${
-              index === path ? "bg-[#A3C09F]" : "hover:bg-[#B9D9B5]"
-            }`}
-          >
-            <h1 className="text-[#185E0E] font-bold text-2xl">{text}</h1>
-          </div>
-        </Link>
-      );
-    } else {
-      return (
-        <div
-          className={`flex w-[180px] items-center justify-center h-full cursor-pointer hover:bg-[#B9D9B5]`}
-          onClick={showContent}
-        >
-          <h1 className="text-[#185E0E] font-bold text-2xl">{text}</h1>
-        </div>
-      );
-    }
-  };
 
   return (
     <>
@@ -53,9 +14,30 @@ export default function Header(props) {
           </h1>
         </div>
 
-        {renderLink("/", "Home")}
-        {renderLink("/current", "Current")}
-        {renderLink("/stats", "MyStats")}
+        <Link
+          className={`flex w-[180px] items-center justify-center h-full cursor-pointer ${
+            index === 0 ? "bg-[#A3C09F]" : "hover:bg-[#B9D9B5]"
+          }`}
+          href="/"
+        >
+          <h1 className="text-[#185E0E] font-bold text-2xl">Home</h1>
+        </Link>
+        <Link
+          className={`flex w-[180px] items-center justify-center h-full cursor-pointer ${
+            index === 1 ? "bg-[#A3C09F]" : "hover:bg-[#B9D9B5]"
+          }`}
+          href={"/current"}
+        >
+          <h1 className="text-[#185E0E] font-bold text-2xl">Current</h1>
+        </Link>
+        <Link
+          className={`flex w-[180px] items-center justify-center h-full cursor-pointer ${
+            index === 2 ? "bg-[#A3C09F]" : "hover:bg-[#B9D9B5]"
+          }`}
+          href={"/stats"}
+        >
+          <h1 className="text-[#185E0E] font-bold text-2xl">MyStats</h1>
+        </Link>
       </div>
     </>
   );
