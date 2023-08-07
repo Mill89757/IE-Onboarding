8d82ea51e6fc61b8aec581d44ab45c314aaa9843
Mill <yushuguo89757@gmail.com>
2023-08-07 17:49:05 +1000

    Feat: Add a switch for switch data source

--- pages/currentDetails.js
+++ pages/currentDetails.js
@@ -18,7 +18,20 @@ export default function CurrentDetails(props) {
       <Header index={1} />
       <div className="h-[calc(100vh_-_80px)]">
         <div className="flex items-center justify-center w-full h-full">
-          <div style={{ width: "80%", height: "60%" }} className="flex-col">
+          <div style={{ width: "80%", height: "90%" }} className="flex-col">
+            <div className="flex items-center justify-center">
+              <label className="text-gray-600 font-medium mr-2">
+                Data Source:
+              </label>
+              <select
+                // value={dataSource}
+                onChange={(e) => setDataSource(e.target.value)}
+                className="py-2 px-4 border rounded-lg bg-white shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              >
+                <option value="gas">Gas</option>
+                <option value="electricity">Electricity</option>
+              </select>
+            </div>
             {receivedData === "Average Electricity Usage" ? (
               <BoxPlot data={data} />
             ) : (
